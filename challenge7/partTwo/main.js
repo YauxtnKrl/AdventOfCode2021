2e9261bd2a5ca12169174a67a96f63272a1a5bf9
Justin <justin@findmyfit.io>
2022-01-04 12:06:06 -0500

    day 7

--- challenge7/partTwo/main.js
+++ challenge7/partTwo/main.js
@@ -1,19 +1,49 @@
 let crabSubs = [16, 1, 2, 0, 4, 2, 7, 1, 2, 14]
 let sum = 0
-let median
+let average
+
 
 function convertTextFile() {
   var fs = require("fs");
-  var text = fs.readFileSync("./input.txt", "utf-8");
+  var text = fs.readFileSync("./../partOne/input.txt", "utf-8");
 
   crabSubs = text.split(",");
 }
-// convertTextFile()
-
-
+convertTextFile()
 
 crabSubs.forEach(sub => {
-sum += sub+sum
+  sum += Number(sub)
 })
 
-console.log(sum/2)
+average = Math.round(Number(sum) / crabSubs.length)
+
+
+function IsAverageEfficient(list) {
+  var mostEfficient
+  var fuelTotal = 0
+  efficientMove = average - 1
+
+  for (let index = 3; index > 0; index--) {
+    list.forEach(sub => {
+      diff = efficientMove - Number(sub)
+      diff = Math.abs(diff)
+
+      sum = (diff / 2) + 0.5
+      fuelTotal += sum * diff
+    })
+
+    if (index === 3) {
+      mostEfficient = fuelTotal
+    }
+
+    if (fuelTotal < mostEfficient) {
+      mostEfficient = fuelTotal
+    }
+
+    fuelTotal = 0
+    efficientMove++
+  }
+
+  console.log(mostEfficient)
+}
+IsAverageEfficient(crabSubs)
